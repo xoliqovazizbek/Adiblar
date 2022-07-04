4e25e1089dc75d25382c265f6c40aa0a2050ba0b
Alovuddin <100945300+alovuddin-kambarov@users.noreply.github.com>
2022-07-04 13:46:08 +0500

    ok

--- app/src/main/java/uz/coder/adiblar/MainActivity.kt
+++ app/src/main/java/uz/coder/adiblar/MainActivity.kt
@@ -2,10 +2,34 @@ package uz.coder.adiblar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import uz.coder.adiblar.databinding.ActivityMainBinding
+import uz.coder.adiblar.models.User
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firebaseFirestore: FirebaseFirestore
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseFirestore = FirebaseFirestore.getInstance()
+
+
+        binding.btnImg.setOnClickListener {
+
+        }
+
+
+
+        binding.save.setOnClickListener {
+
+            val user = User()
+//            firebaseFirestore.collection("users")
+//                .add()
+        }
     }
+
 }
