eb024e88ac29fbf0c58a05c4edc34639ebcf16f0
Alovuddin <100945300+alovuddin-kambarov@users.noreply.github.com>
2022-07-04 14:01:41 +0500

    ok

--- app/src/main/java/uz/coder/adiblar/MainActivity.kt
+++ app/src/main/java/uz/coder/adiblar/MainActivity.kt
@@ -1,10 +1,15 @@
 package uz.coder.adiblar
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.firestore.FirebaseFirestore
 import uz.coder.adiblar.databinding.ActivityMainBinding
 import uz.coder.adiblar.models.User
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,18 +23,36 @@ class MainActivity : AppCompatActivity() {
         firebaseFirestore = FirebaseFirestore.getInstance()
 
 
-        binding.btnImg.setOnClickListener {
-
-        }
 
 
 
-        binding.save.setOnClickListener {
+            binding.btnImg.setOnClickListener {
+                val getImageContent =
+                    registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                        uri ?: return@registerForActivityResult
+                        val ins = contentResolver?.openInputStream(uri)
+                        val file = File(filesDir, "imageNew.jpg")
+                        val absolutePath = file.absolutePath
+                        val fileOutputStream = FileOutputStream(file)
+                        Toast.makeText(this, "file copied to $absolutePath", Toast.LENGTH_LONG).show()
+                        binding.img.setImageURI(uri)
+                        ins?.copyTo(fileOutputStream)
+                        ins?.close()
+                        fileOutputStream.close()
+                    }
+                fun pickImageFromGalleryNew() {
+                    getImageContent.launch("image/*")
+                }
+            }
 
-            val user = User()
+
+
+            binding.save.setOnClickListener {
+
+                val user = User()
 //            firebaseFirestore.collection("users")
 //                .add()
+            }
         }
-    }
 
-}
+    }
